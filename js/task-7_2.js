fbc6e831e57b057c071884c475452aae85239f6b
Vitamin-JS <Vpopchenkov@gmail.com>
2020-09-22 23:27:35 +0300

    Add correct Task 7-2

--- js/task-7_2.js
+++ js/task-7_2.js
@@ -15,3 +15,15 @@ const ingredients = [
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
 //     после чеговставит все li за одну операцию в список ul.ingredients.
 // Для создания DOM - узлов используй document.createElement().
+
+const ingridientsEmpyList = document.querySelector("#ingredients");
+
+const makeElements = ingredients.map((element) => {
+  const createEl = document.createElement("li");
+  createEl.textContent = element;
+  return createEl; // возвращает каждый новый элемент =>  <li>Картошка</li> и т.д.
+});
+//console.log(makeElements); // выводит массив из 6ти <li>
+
+ingridientsEmpyList.append(...makeElements);
+console.log(ingridientsEmpyList);
